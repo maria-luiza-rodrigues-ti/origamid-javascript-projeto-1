2ea45dbc8792957f8f7e70837737b5a50c8e84fe
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-02-05 17:44:58 -0300

    refactor: change anima-numeros structure to class

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,10 +1,9 @@
-import initAnimaNumeros from "./anima-numeros.js";
+import AnimaNumeros from "./anima-numeros.js";
 
 export default function initFetchAnimais() {
   function createAnimal(animal) {
     const div = document.createElement("div");
     div.classList.add("numero-animal");
-
     div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
     return div;
   }
@@ -14,13 +13,16 @@ export default function initFetchAnimais() {
       const animaisResponse = await fetch(url);
       const animaisJSON = await animaisResponse.json();
       const numerosGrid = document.querySelector(".numeros-grid");
-
       animaisJSON.forEach((animal) => {
         const divAnimal = createAnimal(animal);
         numerosGrid.appendChild(divAnimal);
       });
-
-      initAnimaNumeros();
+      const animaNumeros = new AnimaNumeros(
+        "[data-numero]",
+        ".numeros",
+        "ativo"
+      );
+      animaNumeros.init();
     } catch (erro) {
       console.log(erro);
     }
